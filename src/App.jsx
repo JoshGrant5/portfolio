0bc28ed3a977ead5d690ef642c156a86778def0f
JoshGrant5 <joshgg@icloud.com>
2020-12-09 20:07:05 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import './App.scss';
+
+export default function App() {
+  return (
+     <BrowserRouter>
+       <Switch>
+         <Route path="/" exact component={} />
+
+       </Switch>
+     </BrowserRouter>
+  );
+}
