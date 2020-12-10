f6048625b574f60c44a311d7318d178b112255a1
JoshGrant5 <joshgg@icloud.com>
2020-12-09 21:50:43 -0800

    Add project data

---
+++ src/projects.js
@@ -0,0 +1,92 @@
+export const projects = [
+  {
+    id: 1,
+    name: 'TinyApp',
+    description: 'A Bitly-style application allowing users to create shortened URLs.',
+    image: '',
+    stack: {
+      frontEnd: ['EJS', 'HTML5', 'CSS', 'Bootstrap'],
+      backEnd: ['Node.js', 'Express'],
+      testing: ['Mocha', 'Chai'],
+      other: []
+    },
+    date: 'OCT 2020',
+    github: 'https://github.com/JoshGrant5/tinyapp',
+    deployment: ''
+  },
+  {
+    id: 2,
+    name: 'Tweeter',
+    description: 'A single-page Twitter clone.',
+    image: '',
+    stack: {
+      frontEnd: ['JQuery', 'EJS', 'Ajax', 'HTML5', 'CSS'],
+      backEnd: ['Node.js', 'Express'],
+      testing: [],
+      other: []
+    },
+    date: 'OCT 2020',
+    github: 'https://github.com/JoshGrant5/tweeter',
+    deployment: ''
+  },
+  {
+    id: 3,
+    name: 'QuizAndTell',
+    description: 'A Buzzfeed-style quiz app for both trivia and personality quizzes.',
+    image: '',
+    stack: {
+      frontEnd: ['JQuery', 'EJS', 'Ajax', 'HTML5', 'SCSS', 'Bootstrap'],
+      backEnd: ['Node.js', 'Express', 'PostgreSQL'],
+      testing: [],
+      other: []
+    },
+    date: 'OCT 2020',
+    github: 'https://github.com/JoshGrant5/quiz-app',
+    deployment: 'https://quizandtell.herokuapp.com/'
+  },
+  {
+    id: 4,
+    name: 'LHL Scheduler',
+    description: 'A single-page app for users to book interviews in a realtime experience.',
+    image: '',
+    stack: {
+      frontEnd: ['React', 'Axios', 'WebSockets', 'HTML5', 'SCSS'],
+      backEnd: ['Node.js', 'PostgreSQL'],
+      testing: ['Jest', 'Cypress', 'Storybook'],
+      other: []
+    },
+    date: 'OCT 2020',
+    github: 'https://github.com/JoshGrant5/tinyapp',
+    deployment: 'https://lhl-scheduler-jg.netlify.app/'
+  },
+  {
+    id: 5,
+    name: 'Jungle',
+    description: 'A mini e-commerce app.',
+    image: '',
+    stack: {
+      frontEnd: ['ERB', 'HTML5', 'SCSS'],
+      backEnd: ['Ruby on Rails', 'PostgreSQL'],
+      testing: ['RSpec', 'Capybara'],
+      other: ['Stripe API']
+    },
+    date: 'NOV 2020',
+    github: 'https://github.com/JoshGrant5/jungle',
+    deployment: ''
+  },
+  {
+    id: 6,
+    name: 'Symposium',
+    description: 'A video chat platform for like-minded people to discuss podcasts.',
+    image: '',
+    stack: {
+      frontEnd: ['React', 'Axios', 'Simple-Peer', 'HTML5', 'SCSS'],
+      backEnd: ['Node.js', 'Express', 'Axios', 'Socket.io', 'PostgreSQL'],
+      testing: ['Cypress'],
+      other: ['Itunes API']
+    },
+    date: 'OCT 2020',
+    github: 'https://github.com/JoshGrant5/symposium',
+    deployment: 'https://the-symposium.herokuapp.com/'
+  },
+];
