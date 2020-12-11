aeaee11f0319b32740e67b13d3ea5a093565d48a
JoshGrant5 <joshgg@icloud.com>
2020-12-11 15:27:52 -0800

    Leave nav component to style later

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,21 +10,32 @@ export default function Nav(props) {
   };
 
   // Make Tabs act as anchor tags, using props.history to redirect routes
-  useEffect(() => {
-    const paths = ['/projects', '/background', '/contact'];
-    props.history.push(paths[value]);
-  }, [value, props.history])
+  // useEffect(() => {
+  //   const paths = ['/projects', '/background', '/contact'];
+  //   props.history.push(paths[value]);
+  // }, [value, props.history])
 
   return (
     <nav>
       <AppBar id='navbar'>
-        <a href='/' id='logo'> JG </a>
         <Tabs value={value} onChange={handleChange} id='tabs'>
+          <Tab label="JG" id='logo' />
           <Tab label="Projects" className='nav-item' />
           <Tab label="Background" className='nav-item' />
           <Tab label="Contact" className='nav-item' />
         </Tabs>
       </AppBar>
     </nav>
+
+// <nav>
+// <AppBar id='navbar'>
+//   <a href='/' id='logo'> JG </a>
+//   <Tabs value={value} onChange={handleChange} id='tabs'>
+//     <a href='/projects' className='nav-item'> Projects </a>
+//     <a href='/background' className='nav-item'> Background </a>
+//     <a href='/contact' className='nav-item'> Contact </a> 
+//   </Tabs>
+// </AppBar>
+// </nav>
   )
 };
