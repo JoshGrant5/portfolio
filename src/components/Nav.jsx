6628646218b2a3f1fc22f3467da5391562931470
JoshGrant5 <joshgg@icloud.com>
2020-12-11 13:54:46 -0800

    Use material ui AppBar and Tabs for navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,31 @@
+import { AppBar, Tabs, Tab } from '@material-ui/core';
+import { useState } from 'react';
+import '../styles/nav.scss';
+
 export default function Nav(props) {
+  const [value, setValue] = useState(0);
+
+  const propIndex = index => {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <nav>
-  
+      <AppBar id='navbar'>
+        <a href='/' id='logo'> JG </a>
+        <Tabs value={value} onChange={handleChange} id='tabs'>
+          <Tab label="Projects" className='nav-item' {...propIndex(0)} />
+          <Tab label="Background" className='nav-item' {...propIndex(1)} />
+          <Tab label="Contact" className='nav-item' {...propIndex(2)} />
+        </Tabs>
+      </AppBar>
     </nav>
   )
 };
