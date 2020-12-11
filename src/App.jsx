684dfd14bb2b6583545806c6b36cd6b5493b9bb4
JoshGrant5 <joshgg@icloud.com>
2020-12-11 15:12:58 -0800

    Move Nav componenet from Home to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Nav from './components/Nav';
 import Home from './components/Home';
 import Projects from './components/Projects';
 
 import './styles/index.scss';
 
-export default function App() {
+export default function App(props) {
   return (
      <BrowserRouter>
-       <Switch>
-         <Route path="/" exact component={Home} />
-         <Route path="/projects" exact component={Projects} />
-       </Switch>
+      <Nav />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/projects" exact component={Projects} />
+        {/* <Route path="/background" exact component={Background} />
+        <Route path="/contact" exact component={Contact} /> */}
+      </Switch>
      </BrowserRouter>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,19 @@
 import { useState } from 'react';
 import Landing from './Landing';
-import Nav from './Nav';
+// import Nav from './Nav';
 
 export default function Home(props) {
 
   const [landing, setLanding] = useState(false);
 
   window.addEventListener('load', event => {
-    console.log('load');
     setLanding(true);
   });
 
   return (
     <main>
       <Landing />
-      <Nav history={props.history}/>
+      {/* <Nav history={props.history}/> */}
     </main>
   );
 };
