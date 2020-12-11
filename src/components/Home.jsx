0db8326419aad4bd8cbf6765f6a4332fcd963d0a
JoshGrant5 <joshgg@icloud.com>
2020-12-11 12:34:18 -0800

    Install react-transition-group

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,16 @@
+import { useState } from 'react';
 import Landing from './Landing';
 import Nav from './Nav';
 
 export default function Home(props) {
+
+  const [landing, setLanding] = useState(false);
+
+  window.addEventListener('load', event => {
+    console.log('load');
+    setLanding(true);
+  });
+
   return (
     <main>
       <Landing />
