e7dc778889800749ae8195ce931496ec37ac62a7
JoshGrant5 <joshgg@icloud.com>
2020-12-11 14:26:42 -0800

    Nav redirects to appropriate routes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ export default function Home(props) {
   return (
     <main>
       <Landing />
-      <Nav />
+      <Nav history={props.history}/>
     </main>
   );
 };
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,29 +1,28 @@
-import { AppBar, Tabs, Tab } from '@material-ui/core';
-import { useState } from 'react';
+import { AppBar, Tabs, Tab, Link } from '@material-ui/core';
+import { useEffect, useState } from 'react';
 import '../styles/nav.scss';
 
 export default function Nav(props) {
-  const [value, setValue] = useState(0);
-
-  const propIndex = index => {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
-  }
+  const [value, setValue] = useState(); // tracks which Tab has been selected
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
+  // Make Tabs act as anchor tags, using props.history to redirect routes
+  useEffect(() => {
+    const paths = ['/projects', '/background', '/contact'];
+    props.history.push(paths[value]);
+  }, [value, props.history])
 
   return (
     <nav>
       <AppBar id='navbar'>
         <a href='/' id='logo'> JG </a>
         <Tabs value={value} onChange={handleChange} id='tabs'>
-          <Tab label="Projects" className='nav-item' {...propIndex(0)} />
-          <Tab label="Background" className='nav-item' {...propIndex(1)} />
-          <Tab label="Contact" className='nav-item' {...propIndex(2)} />
+          <Tab label="Projects" className='nav-item' />
+          <Tab label="Background" className='nav-item' />
+          <Tab label="Contact" className='nav-item' />
         </Tabs>
       </AppBar>
     </nav>
