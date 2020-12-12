9a2289d206cf5dc019511adaa98648dd05c3a402
JoshGrant5 <joshgg@icloud.com>
2020-12-11 17:05:33 -0800

    Add education data

---
+++ src/data/education.js
@@ -0,0 +1,34 @@
+export const education = [
+  {
+    degree: [
+      {
+        school: 'Lighthouse Labs',
+        program: 'Web Development Bootcamp',
+        type: 'Diploma',
+        date: 'December 2020',
+      },
+      {
+        school: 'Carleton University',
+        program: 'Criminology & Criminal Justice',
+        type: 'Bachelor of Arts Honours',
+        date: 'April 2017',
+      }
+    ]
+  },
+  {
+    course: [
+      {
+        school: 'HarvardX',
+        course: 'CS50: Intro to Computer Science',
+        type: 'Certificate',
+        date: 'August 2020',
+      },
+      {
+        school: 'Udemy',
+        course: 'Zero to Hero in Python 3',
+        type: 'Certificate',
+        date: 'May 2020',
+      },
+    ]
+  }
+];
