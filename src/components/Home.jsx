77967d24c2b352bd519a94c635c30acab25d6be9
JoshGrant5 <joshgg@icloud.com>
2020-12-11 18:53:46 -0800

    Add button from bttn.cs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,15 @@
 import { useState } from 'react';
-import Landing from './Landing';
+// import Landing from './Landing';
 import cropped4 from '../img/cropped4.jpg';
 // import Nav from './Nav';
 
 export default function Home(props) {
 
-  const [landing, setLanding] = useState(false);
+  // const [landing, setLanding] = useState(false);
 
-  window.addEventListener('load', event => {
-    setLanding(true);
-  });
-
+  // window.addEventListener('load', event => {
+  //   setLanding(true);
+  // });
 
 
   return (
@@ -21,9 +20,11 @@ export default function Home(props) {
       <section id='sidebar'></section>
 
       <article id='heading'>
-        <h1>Josh Grant</h1>
-        <h2>Full Stack Developer</h2>
+        <span id='name'>Josh Grant</span>
+        <span id='title'>Full Stack Developer</span>
       </article>
+
+      <button class="bttn-stretch bttn-md bttn-success">View</button>
 
       <img src={cropped4} alt='profile-pic' id='profile-pic' />
    
