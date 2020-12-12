68486ddf86eb35085b202c7e250ddcfaafed00fc
JoshGrant5 <joshgg@icloud.com>
2020-12-11 16:23:00 -0800

    Add Bio component with breif summary of myself

---
+++ src/components/Bio.jsx
@@ -0,0 +1,8 @@
+export default function Bio(props) {
+
+  return (
+    <section>
+      <p>I am a full stack developer who relishes the opportunity to use my creativity and intuition to build meaningful things. My degree in criminology cultivated a critical thinking mindset which I bring with me to each problem I tackle. I am diligent in my pursuit of a solution, and my professional background in sales and customer service instilled in me the values of communication and collaboration. When I'm not programming, I can be found listening to podcasts, enjoying local theatre productions, or helping my beer league softball team to victory.</p>
+    </section>
+  );
+};
