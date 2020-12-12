2cacc773c5e5b4c60dfcee446ed3231a3cd1716d
JoshGrant5 <joshgg@icloud.com>
2020-12-11 16:22:39 -0800

    Add tech skills object

---
+++ src/data/skills.js
@@ -0,0 +1,7 @@
+export const skills = {
+  languages: ['JavaScript', 'Python', 'Ruby', 'C', 'HTML5', 'CSS'],
+  frameworks: ['Node.js', 'React', 'Rails', 'JQuery', 'Axios', 'Express', 'EJS', 'Flask', 'SCSS', 'WebSockets', 'Socket.io', 'WebRTC'],
+  databases: ['PostgreSQL', 'SQLite'],
+  versionControl: ['Git'],
+  testing: ['Mocha', 'Chai', 'Jest', 'Cypress', 'RSpec', 'Capybara']
+};
