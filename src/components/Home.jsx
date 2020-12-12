3e2ac342f26558e76d85387179549b3c3a49e67a
JoshGrant5 <joshgg@icloud.com>
2020-12-11 21:11:28 -0800

    Rename files to make a new home component to render the landing and transition

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,33 +1,19 @@
 import { useState } from 'react';
-// import Landing from './Landing';
-import cropped4 from '../img/cropped4.jpg';
-// import Nav from './Nav';
+import Landing from './Landing';
+import Transition from './Transition';
 
 export default function Home(props) {
 
-  // const [landing, setLanding] = useState(false);
+  // const [transition, setTransition] = useState(false);
 
   // window.addEventListener('load', event => {
-  //   setLanding(true);
+  //   setsTransition(true);
   // });
-
 
   return (
     <main>
-      {/* <Landing /> */}
-      {/* <Bio /> */}
-      
-      <section id='sidebar'></section>
-
-      <article id='heading'>
-        <span id='name'>Josh Grant</span>
-        <span id='title'>Full Stack Developer</span>
-      </article>
-
-      <button class="bttn-stretch bttn-md bttn-success">View</button>
-
-      <img src={cropped4} alt='profile-pic' id='profile-pic' />
-   
+      <Landing />
+      {/* <Transition />  */}
     </main>
   );
 };
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,8 +1,20 @@
-// The visual seen on load of the home page. Inspired by The Athletic and Uber mobile apps
+import cropped4 from '../img/cropped4.jpg';
+
 export default function Landing(props) {
+
   return (
-    <div className='landing'>
-      Josh Grant
+    <div>
+      <aside id='sidebar'></aside>
+
+      <article id='heading'>
+        <span id='name'>Josh Grant</span>
+        <span id='title'>Full Stack Developer</span>
+      </article>
+
+      <button class="bttn-stretch bttn-md bttn-success">View</button>
+
+      <img src={cropped4} alt='profile-pic' id='profile-pic' />
+   
     </div>
   );
-;}
+};
