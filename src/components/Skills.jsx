293e60866a40179a160778e5c1dbd142391806b1
JoshGrant5 <joshgg@icloud.com>
2020-12-12 20:09:47 -0800

    Add grid layout to skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,59 +1,30 @@
 import { skills } from '../data/skills';
+import '../styles/skills.scss';
 
 export default function Skills(props) {
 
-  const languages = skills.languages.map(skill => {
-    return (
-      <ul>
-        <li>{skill}</li>
-      </ul>
-    )
-  });
-
-  const frameworksAndLibraries = skills.frameworks.map(skill => {
-    return (
-      <ul>
-        <li>{skill}</li>
-      </ul>
-    )
-  });
-
-  const databases = skills.databases.map(skill => {
-    return (
-      <ul>
-        <li>{skill}</li>
-      </ul>
-    )
-  });
-
-  const versionControl = skills.versionControl.map(skill => {
-    return (
-      <ul>
-        <li>{skill}</li>
-      </ul>
-    )
-  });
-
-  const testing = skills.testing.map(skill => {
-    return (
-      <ul>
-        <li>{skill}</li>
-      </ul>
-    )
-  });
-
   return (
-    <div className='skills'>
-      <h3>Languages</h3>
-      {languages}
-      <h3>Frameworks & Libraries</h3>
-      {frameworksAndLibraries}
-      <h3>Databases</h3>
-      {databases}
-      <h3>Version Control</h3>
-      {versionControl}
-      <h3>Testing</h3>
-      {testing}
-    </div>
+    <section id='skills'>
+      <div className='container'>
+        <h3>Languages</h3>
+        <p>{skills.languages}</p>
+      </div>
+      <div className='container'>
+        <h3>Frameworks & Libraries</h3>
+        <p>{skills.frameworks}</p>
+      </div>
+      <div className='container'>
+        <h3>Databases</h3>
+        {skills.databases}
+      </div>
+      <div className='container'>
+        <h3>Version Control</h3>
+        {skills.versionControl}
+        </div>
+      <div className='container'>
+        <h3>Testing</h3>
+        {skills.testing}
+      </div>
+  </section>
   );
 };
--- src/data/skills.js
+++ src/data/skills.js
@@ -1,7 +1,7 @@
 export const skills = {
-  languages: ['JavaScript', 'Python', 'Ruby', 'C', 'HTML5', 'CSS'],
-  frameworks: ['Node.js', 'React', 'Rails', 'JQuery', 'Axios', 'Express', 'EJS', 'Flask', 'SCSS', 'WebSockets', 'Socket.io', 'WebRTC'],
-  databases: ['PostgreSQL', 'SQLite'],
-  versionControl: ['Git'],
-  testing: ['Mocha', 'Chai', 'Jest', 'Cypress', 'RSpec', 'Capybara']
+  languages: 'JavaScript, Python, Ruby, C, HTML5, CSS',
+  frameworks: 'Node.js, React, Rails, JQuery, Axios, Express, EJS, Flask, SCSS, WebSockets, Socket.io, WebRTC',
+  databases: 'PostgreSQL, SQLite',
+  versionControl: 'Git',
+  testing: 'Mocha, Chai, Jest, Cypress, RSpec, Capybara'
 };
