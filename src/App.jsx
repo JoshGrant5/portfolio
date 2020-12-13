010b6fdc84284841f42a8b3e199989a134f7b907
JoshGrant5 <joshgg@icloud.com>
2020-12-12 18:08:09 -0800

    Add bio route to react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Nav from './components/Nav';
 import Home from './components/Home';
+import Bio from './components/Bio';
 import Projects from './components/Projects';
 import Background from './components/Background';
 import Contact from './components/Contact';
@@ -13,6 +14,7 @@ export default function App(props) {
       <Nav />
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/bio" exact component={Bio} />
         <Route path="/projects" exact component={Projects} />
         <Route path="/background" exact component={Background} />
         <Route path="/contact" exact component={Contact} />
