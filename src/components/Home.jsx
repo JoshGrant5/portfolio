8fa413ef4e3f79b997e5cc1f9e4b42171be7403e
JoshGrant5 <joshgg@icloud.com>
2020-12-13 17:07:10 -0800

    useSpring to update sideBar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useSpring, config } from 'react-spring';
 import Landing from './Landing';
 import Transition from './Transition';
 import Bio from './Bio';
@@ -8,15 +9,26 @@ export default function Home(props) {
 
   // State for determining whether to render the Landing, Transition, or Bio component. Default to Landing
   const [page, setPage] = useState('LANDING');
+  const [view, setView] = useState(false);
 
   // window.addEventListener('load', event => {
   //   setsTransition(true);
   // });
 
+  const showSideBar = useSpring({
+    config: { ...config.stiff },
+    from: { 
+      width: ''
+    },
+    to: {
+      width: view ? '15vw' : ''
+    }
+  })
+
   return (
     <div>
       <main>
-        {page === 'LANDING' && <Landing setPage={setPage} />}
+        {page === 'LANDING' && <Landing setPage={setPage} showSideBar={showSideBar} setView={setView}/>}
         {page === 'TRANSITION' && <Transition  setPage={setPage} />}
         {page === 'BIO' && <Bio />}
         {page === 'PROJECTS' && <Projects />}
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,17 +1,29 @@
+import { useState } from 'react';
+import { animated } from 'react-spring';
 import cropped4 from '../img/cropped4.jpg';
 
 export default function Landing(props) {
 
+  const animatePage = ()=> {
+    props.setView(true)
+    // setTimeout(() => {
+    //   props.setPage('BIO');
+    // }, 500);
+  };
+
+  
+  
+
   return (
     <div>
-      <aside id='sidebar'></aside>
+      <animated.aside id='sidebar' style={props.showSideBar}></animated.aside>
 
       <article id='heading'>
         <span id='name'>Josh Grant</span>
         <span id='title'>Full Stack Developer</span>
       </article>
 
-      <button class="bttn-stretch bttn-md bttn-success" onClick={() => props.setPage('TRANSITION')}>View</button>
+      <button class="bttn-stretch bttn-md bttn-success" onClick={() => animatePage()} onmouseover={() => props.setView(true)} onmouseout={() => props.setView(false)}>View</button>
 
       <img src={cropped4} alt='profile-pic' id='profile-pic' />
    
