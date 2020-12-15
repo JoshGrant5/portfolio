a2ad7960d575061e9a9a2306f799175abc9c381b
JoshGrant5 <joshgg@icloud.com>
2020-12-14 20:20:49 -0800

    Add link to view on canva and link to download as pdf

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,21 +1,22 @@
 import GetAppIcon from '@material-ui/icons/GetApp';
 import PageviewIcon from '@material-ui/icons/Pageview';
 import cv from '../img/cv.png';
+import cv_download from '../img/cv_download.pdf';
 import '../styles/resume.scss';
 
 export default function Resume(props) {
 
   return (
     <div id="resume">
-      <button class="bttn-stretch bttn-md bttn-success cv-bttn" id='canva' onClick={() => props.setPage('TRANSITION')} >
+      <a href='https://www.canva.com/design/DAEKsksAqk0/6UMd6TfyRqQwsOdEp5ZX8A/view?utm_content=DAEKsksAqk0&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink' className="bttn-stretch bttn-md bttn-success cv-bttn" id='canva'>
         View on Canva
         <PageviewIcon />
-      </button> 
+      </a> 
       <img src={cv} alt='cv' id='cv' />
-      <button class="bttn-stretch bttn-md bttn-success cv-bttn" id='download' onClick={() => props.setPage('TRANSITION')} >
+      <a href={cv_download} download='Josh Grant CV' className="bttn-stretch bttn-md bttn-success cv-bttn" id='download'>
         Download as PDF 
         <GetAppIcon />
-      </button> 
+      </a> 
     </div>
   )
 };
