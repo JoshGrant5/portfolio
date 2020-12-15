bebcda236d50cf54e39dbc09d5a7f5aa0cfa2393
JoshGrant5 <joshgg@icloud.com>
2020-12-15 10:19:21 -0800

    Create MediaIcons component to render on multiple routes

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,4 +1,4 @@
-import About from './About';
+import MediaIcons from './MediaIcons';
 import Skills from './Skills';
 import '../styles/bio.scss';
 import test from '../img/test.png';
@@ -7,16 +7,24 @@ import color from '../img/color.png'
 export default function Bio(props) {
 
   return (
-    <section id='bio'>
+    <div id='bio'>
       {/* <div id='backdrop'></div> */}
 
       <div id='bio-img-container'>
         <img src={color} alt='bio-profile-pic' id='bio-profile-pic' />
       </div>
 
-      <Skills />
+      <section>
+        <p id='summary'>Hi, I'm <span>Josh</span>. I build cool things.</p>
 
-      <p id='summary'>I am a full stack developer who relishes the opportunity to use my creativity and intuition to build meaningful things. My degree in criminology cultivated a critical thinking mindset which I bring with me to each problem I tackle. I am diligent in my pursuit of a solution, and my professional background in sales and customer service instilled in me the values of communication and collaboration. When I'm not programming, I can be found listening to podcasts, enjoying local theatre productions, or helping my beer league softball team to victory.</p>
-    </section>
+        <div id="feature-project">
+          <p>Featured Project</p>
+        </div>
+
+        <Skills />
+      </section>
+
+      <MediaIcons />
+    </div>
   );
 };
