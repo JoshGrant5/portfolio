e40c271e875b1bb3066c83b89ee7303008de0a7e
JoshGrant5 <joshgg@icloud.com>
2020-12-15 15:15:12 -0800

    Center the project name and description

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,30 +7,7 @@ export default function Projects(props) {
   const projectList = projects.map(project => {
     let image = require(`../img/${project.image}`);
 
-    const other = () => {
-      if (project.stack.other) { 
-        return  (
-          <div>
-            <h3>Other</h3>
-            {project.stack.other}
-          </div>
-        )
-      }
-    };
-
-    const testing = () => {
-      if (project.stack.testing) { 
-        return  (
-          <div>
-            <h3>Testing</h3>
-            {project.stack.testing}
-          </div>
-        )
-      }
-    }
-
-    return (
-      
+    return (      
       <div id='project-tl'>
         <div className='tl-item'>
           <div className='tl-bg' >
@@ -38,41 +15,17 @@ export default function Projects(props) {
           </div>
           <div className='line-container'>
             <div className='text-container'>
-              <div className='tl-header'>
-                <h1>{project.name}</h1>
-              </div>
-              <div className='tl-content'>
-                <p>{project.description}</p>
-              </div>
+              <p className='tl-header'>{project.name}</p>
+              <p className='tl-content'>{project.description}</p>
             </div>
             <div className='project-img-container'>
               <img src={image.default} alt='project' className='project-img' /> 
             </div> 
           </div>
-        </div>
-
-       {/*  <div className='container'>
-        <div className='project'>
-          <h1>{project.name} <span>{project.date}</span> </h1>
-          <p>{project.description}</p>
-          <p>{project.github}</p>
-          <p>{project.deployment}</p>
-        </div>
-        <div className='stack'>
-          <h3>Front End</h3>
-            {project.stack.frontEnd}
-          <h3>Back End</h3>
-            {project.stack.backEnd}
-          {other()}
-          {testing()}
-        </div>
-        <div className='project-img'>
-         <img src={image.default} alt='project' className='project-img' /> 
-        </div> */}
-        
+        </div>      
       </div>
-    )
-  })
+    );
+  });
 
   return (
     <section id='projects'>
