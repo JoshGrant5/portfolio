b50b16c9f864001e8f916f2023c3832ed4ede051
JoshGrant5 <joshgg@icloud.com>
2020-12-14 23:26:40 -0800

    Update about card descriptions

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,3 @@
-import { experience } from '../data/experience.js';
-import Education from './Education';
 import coding from '../img/coding.jpg';
 import lighthouse from '../img/lighthouse.jpg';
 import lumber from '../img/lumber.jpg';
@@ -8,31 +6,8 @@ import '../styles/about.scss';
 
 export default function about(props) {
 
-  const roles = experience.map(role => {
-
-    const responsibilities = role.responsibilities.map(duty => <li>{duty}</li>);
-
-    return (
-      <section classNameName='role'>
-        <h1>{role.role} <span>{role.dates}</span> </h1>
-        <h3>{role.company}</h3>
-        <ul>
-          {responsibilities}
-        </ul>
-      </section>
-    )
-  });
-
   return (
-    // <section>
-    //   <div id='background'>
-    //     {roles}
-    //   </div>
-    //   <Education />
-    // </section>
-
     //* Styles inspired by and modified off of design by Jeff Glen https://codepen.io/jeffglenn/pen/KNYoKa/
-
     <section id='timeline'> 
       <div className='tl-item'>
         <div className='tl-bg' >
@@ -40,11 +15,11 @@ export default function about(props) {
         </div>
         <div className='tl-year'>
           <p>Carleton University</p>
-          <p>2013 - 2017</p>
         </div>
         <div className='tl-content'>
-          <h1>Lorem ipsum dolor sit</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit.</p>
+          <h1>4 Years of Criminology</h1>
+          <p>2013 - 2017</p>
+          <p>Living in the Nation's Capital, I earned my Bachelor of Arts degree studying criminology at Carleton University.</p>
         </div>
       </div>
 
@@ -54,11 +29,11 @@ export default function about(props) {
         </div>
         <div className='tl-year'>
           <p>Lumber Industry</p>
-          <p>2017 - 2020</p>
         </div>
         <div className='tl-content'>
-          <h1 className='f3 text--accent ttu'>Vestibulum laoreet lorem</h1>
-          <p>Suspendisse potenti. Sed sollicitudin eros lorem, eget accumsan risus dictum id. Maecenas dignissim ipsum vel mi rutrum egestas. Donec mauris nibh, facilisis ut hendrerit vel, fringilla sed felis. Morbi sed nisl et arcu.</p>
+          <h1 className='f3 text--accent ttu'>All About The 2x4's</h1>
+          <p>2017 - 2020</p> 
+          <p>While attending Carleton I spent my summers working at Dick's Lumber in North Vancouver. After graduating, they gave me an opportunity in contract sales. I'd spend the next 3 years in the lumber industry in sales, transitioning to commodity trading in late 2019 to work as a lumber trader for Boscus Canada.</p>
         </div>
       </div>
 
@@ -68,11 +43,12 @@ export default function about(props) {
         </div>
         <div className='tl-year'>
           <p>Lighthouse Labs</p>
-          <p>2020</p>
+          {/* <p>2020</p> */}
         </div>
         <div className='tl-content'>
-          <h1 className='f3 text--accent ttu'>Phasellus mauris elit</h1>
-          <p>Mauris cursus magna at libero lobortis tempor. Suspendisse potenti. Aliquam interdum vulputate neque sit amet varius. Maecenas ac pulvinar nisi. Fusce vitae nunc ultrices, tristique dolor at, porttitor dolor. Etiam id cursus arcu, in dapibus nibh. Pellentesque non porta leo. Nulla eros odio, egestas quis efficitur vel, pretium sed.</p>
+          <h1 className='f3 text--accent ttu'>This Is Where The Fun Begins</h1>
+          <p>2020</p>
+          <p>After years of thinking of coding through the lense of, "that's cool, I wonder if I'd be able to do that", I finally made the plunge. HarvardX CS50 made me fall in love with programming and dream of becoming a developer. I enrolled in Lighthouse Labs Web Development Bootcamp to make that dream a reality. Three months later, my love for programming has only grown. And I'm just getting started.</p>
         </div>
       </div>
 
@@ -84,8 +60,9 @@ export default function about(props) {
           <p>Present Day</p>
         </div>
         <div className='tl-content'>
-          <h1 className='f3 text--accent ttu'>Mauris vitae nunc elem</h1>
-          <p>Suspendisse ac mi at dolor sodales faucibus. Nunc sagittis ornare purus non euismod. Donec vestibulum efficitur tortor, eget efficitur enim facilisis consequat. Vivamus laoreet laoreet elit. Ut ut varius metus, bibendum imperdiet ex. Curabitur diam quam, blandit at risus nec, pulvinar porttitor lorem. Pellentesque dolor elit.</p>
+          <h1 className='f3 text--accent ttu'>To Infinity</h1>
+          <p>2020 & Beyond</p>
+          <p>I'm currently looking for my first junior developer role to break into the industry with. I can't wait for that opportunity.</p>
         </div>
       </div>
     </section>
