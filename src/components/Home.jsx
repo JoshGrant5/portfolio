505b7d0812dc257dbe23bf7e4e462e0ee03b67d1
JoshGrant5 <joshgg@icloud.com>
2020-12-16 22:14:39 -0800

    Update project descriptions to include more info

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import MediaIcons from './MediaIcons';
 import Skills from './Skills';
-import '../styles/bio.scss';
+import '../styles/home.scss';
 import color from '../img/color.png'
 
 export default function Home(props) {
@@ -13,7 +13,7 @@ export default function Home(props) {
       </div>
 
       <section>
-        <p id='summary'>Hi, I'm <span>Josh</span>. I build cool things.</p>
+        <p id='summary'>Hi, I'm <span>Josh</span>. I'm a <span>25</span> year old web developer from Vancouver, BC. I'm a terrible dancer, I sometimes oh... sorry, wrong convo... I'm a proud bootcamp grad,  </p>
         <p>My Skills</p>
         <Skills />
       </section>
