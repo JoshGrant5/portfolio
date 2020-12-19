c3b773dd3c0c6f0ffc1c26417aca3d6259effdcc
JoshGrant5 <joshgg@icloud.com>
2020-12-19 14:39:33 -0800

    Fix bug where projects look different on Safari

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,7 +1,7 @@
 export const skills = {
   languages: 'JavaScript, Python, Ruby, C, HTML5, CSS',
-  frameworks: 'Node.js, React, Rails, JQuery, Axios, Express, EJS, Flask, SCSS, WebSockets, Socket.io, WebRTC',
+  frameworks: 'Node.js, React, Rails, JQuery, Axios, Express, EJS, Flask, SCSS, WebSockets, Socket.io, WebRTC, Material UI, Bootstrap',
   databases: 'PostgreSQL, SQLite',
   versionControl: 'Git',
-  testing: 'Mocha, Chai, Jest, Cypress, RSpec, Capybara'
+  testing: 'Mocha, Chai, Jest, Cypress, Storybook, RSpec, Capybara'
 };
