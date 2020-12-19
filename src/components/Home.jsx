ad2553017785fbd21d324b3e95f44144c1cd4a88
JoshGrant5 <joshgg@icloud.com>
2020-12-18 22:04:54 -0800

    Add touch icon to buttons on homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,6 @@
+import { NavLink } from 'react-router-dom';
 import Skills from './Skills';
+import TouchAppIcon from '@material-ui/icons/TouchApp';
 import '../styles/home.scss';
 
 export default function Home() {
@@ -12,10 +14,21 @@ export default function Home() {
         <p className='summary'>I'm a <span>25</span> year-old web developer from <span>North Vancouver</span>, BC.</p>
         <p className='summary'>I've been a <span>criminology</span> student, a contract <span>salesman</span>, a <span>lumber trader</span>, and now a <span>code junkie</span>!</p>
         <p className='summary'>I haven't been programming long, but I've finally found what I <span>love</span> to do.</p>
-        <p className='summary'>I build <span>awesome things</span> from the ground up. That's pretty damn cool.</p>
+        <p className='summary'>I build <span>awesome things</span> from the ground up, and that's pretty damn cool.</p>
+        <div className='home-bttns'>
+          <NavLink to="/about" activeClassName='is-selected' className='bttn-stretch bttn-md bttn-success about-bttn'>
+            Get to know me a bit more! 
+            <TouchAppIcon />
+          </NavLink>
+          <NavLink to="/projects" activeClassName='is-selected' className='bttn-stretch bttn-md bttn-success project-bttn'>
+            Check out what I've built so far!
+            <TouchAppIcon />
+          </NavLink>
+        </div>
         <p className='summary stack'><span> {'< My Stack />'} </span></p>
         <Skills />
       </section>
     </div>
   );
 };
+
