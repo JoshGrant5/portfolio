b83a9ae30e2649a2de2d6bcbed52434841f7d197
JoshGrant5 <joshgg@icloud.com>
2020-12-18 19:43:35 -0800

    Increase margins on summary and center-align My Skills tag

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,9 +13,8 @@ export default function Home() {
         <p className='summary'>I've been a <span>criminology</span> student, a contract <span>salesman</span>, a <span>lumber trader</span>, and now a <span>code junkie</span>!</p>
         <p className='summary'>I haven't been programming long, but I've finally found what I <span>love</span> to do.</p>
         <p className='summary'>I build <span>awesome things</span> from the ground up. That's pretty damn cool.</p>
-        <p className='summary'><span>My Stack</span></p>
+        <p className='summary stack'><span> {'< My Stack />'} </span></p>
         <Skills />
-        <p className='summary'><span>Thanks for stopping by!</span></p>
       </section>
     </div>
   );
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,23 +5,23 @@ export default function Skills(props) {
   return (
     <div id='skills'>
       <div className='container'>
-        <h3>Languages</h3>
+        <h3 className='tech-title'>Languages</h3>
         <p>{skills.languages}</p>
       </div>
       <div className='container'>
-        <h3>Frameworks, Libraries & Environments</h3>
+        <h3 className='tech-title'>Frameworks, Libraries & Environments</h3>
         <p>{skills.frameworks}</p>
       </div>
       <div className='container'>
-        <h3>Databases</h3>
+        <h3 className='tech-title'>Databases</h3>
         {skills.databases}
       </div>
       <div className='container'>
-        <h3>Version Control</h3>
+        <h3 className='tech-title'>Version Control</h3>
         {skills.versionControl}
         </div>
       <div className='container'>
-        <h3>Testing</h3>
+        <h3 className='tech-title'>Testing</h3>
         {skills.testing}
       </div>
   </div>
