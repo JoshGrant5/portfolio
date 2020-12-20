4a9afeff49990dd662ec01edafdb7c9ec81afa0a
JoshGrant5 <joshgg@icloud.com>
2020-12-20 12:39:43 -0800

    Fix button positioning when viewport shrinks

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -5,7 +5,6 @@ import cv_download from '../img/cv_download.pdf';
 import '../styles/resume.scss';
 
 export default function Resume(props) {
-
   return (
     <div id="resume">
       <a href='https://www.canva.com/design/DAEKsksAqk0/6UMd6TfyRqQwsOdEp5ZX8A/view?utm_content=DAEKsksAqk0&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink' target="_blank" rel="noopener noreferrer" className="bttn-stretch bttn-md bttn-success cv-bttn" id='canva'>
